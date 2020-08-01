31dcf899316fdd56d45b19ef560e36d17b1607a9
stanley akyea <stanley.akyea@gmail.com>
2020-08-01 23:31:53 +0200

    Define a before_action for setting a portfolio item

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,7 +1,8 @@
 class PortfoliosController < ApplicationController
+	before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
 
 	layout "portfolio"
-	
+
 	def index
 		@portfolio_items = Portfolio.all
 	end
@@ -24,15 +25,12 @@ class PortfoliosController < ApplicationController
 	end
 
 	def show
-		@portfolio_item = Portfolio.find(params[:id])
 	end
 
 	def edit
-		@portfolio_item = Portfolio.find(params[:id])
 	end
 
 	def update
-		@portfolio_item = Portfolio.find(params[:id])
 		respond_to do |format|
 			if @portfolio_item.update(portfolio_params)
 				format.html { redirect_to portfolios_path, notice: 'Item was successfully updated.' }
@@ -43,7 +41,6 @@ class PortfoliosController < ApplicationController
 	end
 
 	def destroy
-		@portfolio_item = Portfolio.find(params[:id])
 		@portfolio_item.destroy
 		respond_to do |format|
 			format.html { redirect_to portfolios_path, notice: 'Item was successfully deleted.' }
@@ -54,6 +51,10 @@ class PortfoliosController < ApplicationController
 	def portfolio_params
 		params.require(:portfolio).permit(:title, :subtitle, :body,
 			technologies_attributes: [:name])
+	end
+
+	def set_portfolio_item
+		@portfolio_item = Portfolio.find(params[:id])
 	end
 
 end
