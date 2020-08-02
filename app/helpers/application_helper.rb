216bf0d3c5f2b0e3a4bf530b3730c6f66d8d20bd
stanley akyea <stanley.akyea@gmail.com>
2020-08-02 16:23:59 +0200

    Integrate bolad-view-gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    @copyright = BoladViewTool::Renderer.copyright 'Stanley Akyea', 'All rights reserved'
+  end
 end
