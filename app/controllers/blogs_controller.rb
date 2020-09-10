85995111ce9e290f1a4efaaa990445d2a5265a1a
stanley akyea <stanley.akyea@gmail.com>
2020-09-10 14:12:11 +0200

    Build comment form for blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -15,6 +15,9 @@ class BlogsController < ApplicationController
 
   # GET /blogs/1
   def show
+    @blog = Blog.includes(:comments).friendly.find(params[:id])
+    @comment = Comment.new
+
     @page_title = @blog.title
     @seo_keywords = @blog.body
   end
@@ -46,7 +49,7 @@ class BlogsController < ApplicationController
       flash[:success] = 'Blog was successfully updated.'
       redirect_to @blog
     else
-      render :edit 
+      render :edit
     end
   end
 
