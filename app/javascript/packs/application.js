f4fd5b06137b135d1d0caf4cfe12de262ab2f578
stanley akyea <stanley.akyea@gmail.com>
2020-09-17 00:27:59 +0200

    Configure application stylesheet path via webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@ import 'bootstrap'
 import 'cocoon-js'
 import "controllers"
 
+import '../stylesheets/application.scss'
+
 require("trix")
 require("@rails/actiontext")
 require("gritter/js/jquery.gritter.js")
@@ -15,6 +17,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery-ui-dist/jquery-ui");
 require("html.sortable")
+require("jquery")
 
 global.$ = jQuery;
 
@@ -55,18 +58,4 @@ ready = function() {
 
 $(document).ready(ready);
 
-// import Sortable from 'sortablejs';
-//
-// document.addEventListener('turbolinks:load', () => {
-//   var el = document.getElementById('task-list');
-//   var sortable = Sortable.create(el);
-// })
 
-
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
